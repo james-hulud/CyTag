ea1846b8290ff36c59f2a96a01969dccdda74e29
james <jamesmichaeldavies@outlook.com>
2025-03-27 18:01:00 +0000

    minor change

--- CyTag_Server.py
+++ CyTag_Server.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/tag_text', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def tag_text():
 	print("Server running")
 	return "<p>hello world</p>"
